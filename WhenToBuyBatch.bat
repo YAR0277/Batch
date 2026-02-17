4761ca41ceb185257162a59f13a8846578215855
dgore03 <davidgore@protonmail.com>
2026-02-17 09:11:19 -0500

    Initial commit

---
+++ WhenToBuyBatch.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\GNU Octave\Octave-9.4.0\mingw64\bin\octave-cli.exe" --silent "C:\Users\drdav\Projects\Octave\Test\WhenToBuyBatch.m" > "C:\Users\drdav\Projects\Batch\WhenToBuyBatchLog.txt" 2>&1
