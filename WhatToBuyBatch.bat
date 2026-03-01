493cdbb31ae1368b4ad6c1d07f0d62c8f8629aab
dgore03 <davidgore@protonmail.com>
2026-03-01 14:34:37 -0500

    Updated from Octave 9.4.0 to Octave 11.1.0

--- WhatToBuyBatch.bat
+++ WhatToBuyBatch.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\GNU Octave\Octave-9.4.0\mingw64\bin\octave-cli.exe" --silent "C:\Users\drdav\Projects\Octave\Test\WhatToBuyBatch.m" > "C:\Users\drdav\Projects\Batch\WhatToBuyBatchLog.txt" 2>&1
+"C:\Program Files\GNU Octave\Octave-11.1.0\mingw64\bin\octave-cli.exe" --silent "C:\Users\drdav\Projects\Octave\Test\WhatToBuyBatch.m" > "C:\Users\drdav\Projects\Batch\WhatToBuyBatchLog.txt" 2>&1
