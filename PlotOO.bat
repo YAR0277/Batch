b78a91bc33ad59eaee5402c0d80a80c4f9226a60
dgore03 <davidgore@protonmail.com>
2026-04-15 17:22:05 -0400

    Added Plot00.bat

---
+++ PlotOO.bat
@@ -0,0 +1,4 @@
+@echo off
+set project_path="C:\Users\drdav\Projects\Octave\Test"
+
+"C:\Program Files\GNU Octave\Octave-9.4.0\mingw64\bin\octave.exe" --persist --eval "graphics_toolkit('qt'); addpath('%project_path%'); DoPlotOO('%1')"
