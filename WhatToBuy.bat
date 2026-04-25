3895940e6e80b4937f0ec8521ee88b994270e107
dgore03 <davidgore@protonmail.com>
2026-04-25 16:16:39 -0400

    Added PlotBB.bat, renamed WhatToBuy.bat in PlotMM.bat

--- WhatToBuy.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set project_path="C:\Users\drdav\Projects\Octave\Test"
-
-"C:\Program Files\GNU Octave\Octave-9.4.0\mingw64\bin\octave.exe" --persist --eval "graphics_toolkit('qt'); addpath('%project_path%'); DoWhatToBuy('%1')"
