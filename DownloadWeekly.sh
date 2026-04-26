86bcbe59e81aa409c1d9f337728aefa5032bc20b
YAR0277 <davidgore@protonmail.com>
2026-04-26 16:11:52 -0400

    Added weekly download for all data

---
+++ DownloadWeekly.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Stop if any command fails
+set -e
+
+# Activate Python virtual environment
+source /home/david/Projects/Python/.venv/bin/activate
+
+echo "Download ETF ..."
+python /home/david/Projects/Python/download_etf_weekly.py
+sleep 1
+
+echo "Download Equity ..."
+python /home/david/Projects/Python/download_equity_weekly.py
+sleep 1
+
+echo "Download Index ..."
+python /home/david/Projects/Python/download_index_weekly.py
+
