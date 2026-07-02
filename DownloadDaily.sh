463b73c7fab968de3ac43ed01751e3bec274ad2e
YAR0277 <davidgore@protonmail.com>
2026-07-02 14:43:27 -0400

    Refactored download bash scripts

--- DownloadDaily.sh
+++ DownloadDaily.sh
@@ -6,14 +6,4 @@ set -e
 # Activate Python virtual environment
 source /home/david/Projects/Python/.venv/bin/activate
 
-echo "Download ETF ..."
-python /home/david/Projects/Python/download_etf.py
-sleep 1
-
-echo "Download Equity ..."
-python /home/david/Projects/Python/download_equity.py
-sleep 1
-
-echo "Download Index ..."
-python /home/david/Projects/Python/download_index.py
-
+python /home/david/Projects/Python/download_daily.py
